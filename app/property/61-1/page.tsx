3eda2fefe1adb7b5cf168ba21762d6ee38bc03d8
jrwishart93 <jrwishart@hotmail.co.uk>
2025-11-07 22:26:48 +0000

    new page
    page 61

---
+++ app/property/61-1/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <main style={{ padding: "2rem" }}>
+      <h1>61/1 Sub-Tenancy Agreement</h1>
+      <p>This page has been created. You can now paste your full HTML agreement here.</p>
+    </main>
+  );
+}
