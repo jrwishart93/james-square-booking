3663190b3730a82119fa03afe28505abc5aa7f08
jrwishart93 <jrwishart@hotmail.co.uk>
2026-01-02 14:48:27 +0000

    Ensure R3F JSX types load via next-env

---
+++ next-env.d.ts
@@ -0,0 +1,15 @@
+/// <reference types="next" />
+/// <reference types="next/image-types/global" />
+
+// NOTE: This file should not be edited
+// see https://nextjs.org/docs/basic-features/typescript for more information.
+
+import { ThreeElements } from "@react-three/fiber";
+
+declare global {
+  namespace JSX {
+    interface IntrinsicElements extends ThreeElements {}
+  }
+}
+
+export {};
